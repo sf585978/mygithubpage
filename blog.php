ce8d95ab1a598ea34b3a8042c66cc2be93166412
Sean Fischer <sf585978@gmail.com>
2015-05-22 16:12:23 -0400

    Test Upload
    Uploading some test files

---
+++ blog.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Basic Page Needs
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <meta charset="utf-8">
+    <title>Sean A. Fischer</title>
+    <meta name="description" content="The personal webpage of Sean A. Fischer.">
+    <meta name="author" content="Sean A. Fischer">
+    <!-- Mobile Specific Metas
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- FONT
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <link href='//fonts.googleapis.com/css?family=Raleway:400,300,600' rel='stylesheet' type='text/css'>
+    <!-- CSS
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <link rel="stylesheet" href="css/skeleton.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/custom.css">
+    <!-- Scripts
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <!-- Favicon
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  </head>
+  <body>
+    <!-- Primary Page Layout
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <?php include('topmatter.php'); ?>
+    <div class="blog">
+      <div class="container">
+        <div class="row">
+          <div class="u-full-width">
+            <h1>Blog</h1>
+          </div>
+        </div>
+        <div class="row">
+          <div class="u-full-width">
+            <ul>
+              <li><a href="" title="">Post 1</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
---
+++ portfolio.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Basic Page Needs
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <meta charset="utf-8">
+    <title>Sean A. Fischer</title>
+    <meta name="description" content="The personal webpage of Sean A. Fischer.">
+    <meta name="author" content="Sean A. Fischer">
+    <!-- Mobile Specific Metas
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- FONT
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <link href='//fonts.googleapis.com/css?family=Raleway:400,300,600' rel='stylesheet' type='text/css'>
+    <!-- CSS
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <link rel="stylesheet" href="css/skeleton.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/custom.css">
+    <!-- Scripts
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <!-- Favicon
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  </head>
+  <body>
+    <!-- Primary Page Layout
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <?php include('topmatter.php'); ?>
+    <div class="portfolio">
+      <div class="container">
+        <div class="row">
+          <div class="u-full-width">
+            <h1>Portfolio</h1>
+          </div>
+        </div>
+        <div class="row">
+          <div class="u-full-width">
+            <h5>Writing</h5>
+          </div>
+        </div>
+        <div class="row">
+          <div class="u-full-width">
+            <h6>Journal Articles</h6>
+          </div>
+        </div>
+        <div class="row">
+          <div class="u-full-width">
+            <ul>
+              <li><a href="" title="">"From Bakhtin to Skyrim" - <em>Metamorphosis</em>, Spring 2014</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="row">
+          <div class="u-full-width">
+            <h5>Code</h5>
+          </div>
+        </div>
+        <div class="row">
+          <div class="u-full-width">
+            <h6>R</h6>
+          </div>
+        </div>
+        <div class="row">
+          <div class="u-full-width">
+            <ul>
+              <li><a href="" title="">CanonicityR</a></li>
+              <li><a href="" title="">Semester Project MATH 340</a></li>
+              <li><a href="" title="">Semester Project BIOL 288</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="row">
+          <div class="u-full-width">
+            <h6>MATLAB</h6>
+          </div>
+        </div>
+        <div class="row">
+          <div class="u-full-width">
+            <ul>
+              <li><a href="" title="">Final Projects MATH 230</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
